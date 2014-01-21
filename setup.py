826efdf862fbad9ce205ee023904ee3628933008
Ken Kundert <thenurd@nurdletech.com>
2014-01-21 01:35:51 -0800

    Renamed main file.

--- setup.py
+++ setup.py
@@ -12,35 +12,37 @@ def get_long_description():
             contents += [f.read()]
     return '\n\n'.join(contents)
 
-setup(
-    name='abraxas',
-    description="password generator",
-    long_description=get_long_description(),
-    author="Kale & Ken Kundert",
-    author_email='abraxas@nurdletech.com',
-    version=VERSION,
-    download_url='git@github.com:KenKundert/abraxas.git',
-    scripts=['abraxas.py'],
-    packages=['abraxas'],
-    py_modules=['fileutils'],
-    data_files=[
-        ('', ['words']),
-        ('man/man1', ['abraxas.1']),
-        ('man/man3', ['abraxas.3']),
-        ('man/man5', ['abraxas.5']),
-    ],
-    license='GPLv3',
-    platforms=['linux'],
-    classifiers=[
-        "Development Status :: 5 - Production/Stable",
-        "Environment :: Console",
-        "Topic :: Utilities",
-        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
-        "Natural Language :: English",
-        "Operating System :: POSIX :: Linux",
-        "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3.3",
-    ]
-)
+def main():
+    setup(
+        name='abraxas',
+        description="password generator",
+        long_description=get_long_description(),
+        author="Kale & Ken Kundert",
+        author_email='abraxas@nurdletech.com',
+        version=VERSION,
+        download_url='git@github.com:KenKundert/abraxas.git',
+        scripts=['main.py'],
+        packages=['abraxas'],
+        py_modules=['fileutils'],
+        data_files=[
+            ('', ['words']),
+            ('man/man1', ['abraxas.1']),
+            ('man/man3', ['abraxas.3']),
+            ('man/man5', ['abraxas.5']),
+        ],
+        license='GPLv3',
+        platforms=['linux'],
+        classifiers=[
+            "Development Status :: 5 - Production/Stable",
+            "Environment :: Console",
+            "Topic :: Utilities",
+            "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+            "Natural Language :: English",
+            "Operating System :: POSIX :: Linux",
+            "Programming Language :: Python :: 2.7",
+            "Programming Language :: Python :: 3.3",
+        ]
+    )
 
+main()
 # vim: set sw=4 sts=4 et:
